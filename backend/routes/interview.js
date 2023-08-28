60d509089aff60b044bc9f6366b6df1f77135545
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-08-28 09:54:04 -0400

    require auth middle ware

--- backend/routes/interview.js
+++ backend/routes/interview.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { postInterview, fetchInterviews } = require('../controllers/interviewController');
+const requireAuth = require('../middleware/requireAuth');
 
 
 const router = express.Router();
 
+router.use(requireAuth)
 router.post('/', postInterview).get('/', fetchInterviews)
 
 
