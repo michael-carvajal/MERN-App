8f2cf07d5d155d86dff16cc6f44ef14def5f67a5
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-08-28 09:16:37 -0400

    sign up and logout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,14 @@
 import { Link } from 'react-router-dom'
+import { useAuthContext } from '../hooks/useAuthContext'
+import  useLogout  from '../hooks/useLogout';
 
 const Navbar = () => {
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+  console.log(user);
+  const handleLogout = () => {
+    logout()
+  }
 
   return (
     <header>
@@ -8,6 +16,10 @@ const Navbar = () => {
         <Link to="/">
           <h1>CEO Chronicles</h1>
         </Link>
+        <div>
+
+        <button className='logout' onClick={handleLogout}>Log out</button>
+        </div>
         <nav>
 
           <Link to="/signup">
--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,15 +1,16 @@
 import { useAuthContext } from "./useAuthContext";
-
-export const useLogout = () => {
+const useLogout = () => {
+    const { dispatch } = useAuthContext();
 
 
     const logout = () => {
         //remove user from storage
         localStorage.removeItem('user');
-        const { dispatch } = useAuthContext();
 
         dispatch({type: 'LOGOUT'})
     }
 
     return {logout}
 }
+
+export default useLogout
