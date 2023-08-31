abc6554ce877b19a43f4739d8df9010272157e92
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-08-30 22:18:52 -0400

    turn home page to tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,40 +2,40 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // pages & components
-// import Home from './pages/Home'
+import Home from './pages/Home'
 import Navbar from './components/Navbar'
 import Signup from './pages/Signup';
 import Login from './pages/Login';
 // import InterviewForm from './pages/InterviewForm';
 
-// <div className="pages">
-//   <Routes>
-//     <Route
-//       path="/"
-//       element={<Home />}
-//     />
 //     <Route
 //       path="/new/interview"
 //       element={<InterviewForm />}
 //     />
 //   </Routes>
-// </div>
 const App: React.FC = () => {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route
-            path="/signup"
-            element={<Signup />}
-          />
+        <div className="pages">
+          <Routes>
+            <Route
+              path="/signup"
+              element={<Signup />}
+            />
 
-          <Route
-            path="/login"
-            element={<Login />}
-          />
-        </Routes>
+            <Route
+              path="/login"
+              element={<Login />}
+            />
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+          </Routes>
+        </div>
       </BrowserRouter>
     </div>
   );
