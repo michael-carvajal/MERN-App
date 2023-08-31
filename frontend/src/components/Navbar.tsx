6fc1056763aec687e738c36e3ece799dec27f496
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-08-31 16:40:48 -0400

    finished navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -13,8 +13,8 @@ const Navbar: React.FC = () => {
   return (
     <header className='w-full flex justify-between p-3'>
 
-        <Link to="/">
-          <h1>CEO Chronicles</h1>
+        <Link to="/" className='font-bold text-3xl hover:text-gray-800'>
+          CEO Chronicles
         </Link>
         <nav>
           {user && <div>
@@ -28,11 +28,11 @@ const Navbar: React.FC = () => {
           {!user && (
             <div className='flex gap-2'>
 
-              <Link to="/signup">
-                <h1>Sign Up</h1>
+            <Link to="/signup" className="bg-white hover:bg-gray-300 text-gray-800  py-2 px-4 border border-gray-400 rounded shadow">
+                Sign Up
               </Link>
-              <Link to="/login">
-                <h1>Log In</h1>
+            <Link to="/login" className="bg-white hover:bg-gray-300 text-gray-800  py-2 px-4 border border-gray-400 rounded shadow">
+                Log In
               </Link>
             </div>
           )}
