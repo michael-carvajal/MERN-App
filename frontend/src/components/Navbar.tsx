569cc49558fa0bcbf279c5ad2c567338bf796698
michael-carvajal <michaelcarvajal.work@gmail.com>
2023-09-04 19:54:13 -0400

    responsive design for navbR

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -11,35 +11,46 @@ const Navbar: React.FC = () => {
   }
 
   return (
-    <header className='w-full flex justify-between p-3'>
-
-        <Link to="/" className='font-bold text-3xl hover:text-gray-800'>
-          CEO Chronicles
-        </Link>
-        <nav>
-          {user && <div>
-            <Link to="/new/interview">
-              <h1>Publish +</h1>
+<header className="w-full flex flex-col md:flex-row justify-between p-3 md:w-3/4 my-0 mx-auto items-center">
+      <Link to="/" className="font-bold text-3xl hover:text-gray-800">
+        CEO Chronicles
+      </Link>
+      <nav className="mt-3 md:mt-0 flex flex-wrap gap-2">
+        {user && (
+          <div className="flex gap-2 items-center">
+            <Link to="/new/interview" className="text-xl hover:text-gray-800">
+              Publish +
             </Link>
-            {user.email}
-          <button className=' text-slate-400 customButton' onClick={handleLogout}>Log out</button>
-          </div>}
+            <span className="text-gray-600">{user.email}</span>
+            <button
+              className="text-slate-400 customButton"
+              onClick={handleLogout}
+            >
+              Log out
+            </button>
+          </div>
+        )}
 
-          {!user && (
-            <div className='flex gap-2'>
-
-            <Link to="/signup" className="bg-white hover:bg-gray-300 text-gray-800  py-2 px-4 border border-gray-400 rounded shadow">
-                Sign Up
-              </Link>
-            <Link to="/login" className="bg-white hover:bg-gray-300 text-gray-800  py-2 px-4 border border-gray-400 rounded shadow">
-                Log In
-              </Link>
-            </div>
-          )}
-        </nav>
-
+        {!user && (
+          <div className="flex justify-between w-[211px]">
+            <Link
+              to="/signup"
+              className="bg-white hover:bg-gray-300 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow "
+            >
+              Sign Up
+            </Link>
+            <Link
+              to="/login"
+              className="bg-white hover:bg-gray-300 text-gray-800 py-2 px-4 border border-gray-400 rounded shadow "
+            >
+              Log In
+            </Link>
+          </div>
+        )}
+      </nav>
     </header>
-  )
+  );
+
 }
 
 export default Navbar
